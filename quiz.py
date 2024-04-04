1cd2d13bfa4557d033d2896f4ac7b252b8e6e819
Athul Ashoan <athulashokdev15gmail.com>
2024-04-04 12:37:32 -0700

    quiz game

---
+++ quiz.py
@@ -0,0 +1,17 @@
+
+
+
+print("quiz game")
+a=input("are you interest play game?: ")
+if  a !='yes':
+    quit
+else:
+    print("lets play")    
+
+answer=input('wats is full form of oops? ')
+
+if answer == "object oriented programing":
+    print('correct')
+else:
+    print("not currect")
+
